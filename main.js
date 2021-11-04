664c50899aa03a41b43bbb1b20607e919307eafd
ThiagoSaviR <thiagosavir@gmail.com>
2021-11-04 09:44:58 -0300

    Ajuste nos icons, cria depoimentos inicial contatos

--- main.js
+++ main.js
@@ -26,3 +26,14 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+// TESTIMONIALS SLIDER SWIPER
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+});
